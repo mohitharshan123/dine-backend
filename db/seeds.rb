28bc39c49c80f906ecdf553275e7a63c04e691d6
Mohit <mohitharshan.mec@gmail.com>
2023-05-01 12:13:57 +0530

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
+require 'faker'
+
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -6,4 +8,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times { Restaurant.create!(name: Faker::Lorem.word) }
+10.times { Restaurant.create!(name: "Swiggy") }
